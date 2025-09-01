7250d04deca8676aae949988a04e10641119836d
Faisal Chishti <fchishti@juniper.net>
2025-09-02 01:04:57 +0530

    Add default values for variables in variable.tf

--- pack/tofu/variable.tf
+++ pack/tofu/variable.tf
@@ -7,6 +7,7 @@
 variable "leaf_model" {
   type = string
   description = "Supported options are QFX5120_48YM, QFX5700, PTX10001_36MR, QFX5130_32CD, ACX7100_48L"
+  default = "QFX5700"
 
   validation {
     condition     = var.leaf_model == "QFX5120_48YM" || var.leaf_model == "QFX5700" || var.leaf_model == "PTX10001_36MR" || var.leaf_model == "QFX5130_32CD" || var.leaf_model == "ACX7100_48L"
@@ -29,6 +30,7 @@ locals {
 variable "fabric_asn_range" {
   type    = string
   description = "Internal Fabric ASN range, i.e. 65000-65010"
+  default = "65000-65010"
 
   validation {
     condition = (length(regexall("^[0-9]+-[0-9]+$", var.fabric_asn_range))==1)
@@ -39,6 +41,7 @@ variable "fabric_asn_range" {
 variable "external_asn_range" {
   type    = string
   description = "External Device ASN range, i.e. 65000-65010"
+  default = "65000-65010"
 
   validation {
     condition = (length(regexall("^[0-9]+-[0-9]+$", var.external_asn_range))==1)
@@ -49,6 +52,7 @@ variable "external_asn_range" {
 variable "ipv4_fabric_loopback_pool" {
   type    = string
   description = "IPv4 loopback address pool for the fabric, i.e. 192.168.10.0/24"
+  default = "192.168.10.0/24"
 
   validation {
     condition      = (length(regexall("^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/[0-9]{1,2}$", var.ipv4_fabric_loopback_pool))==1)
@@ -59,6 +63,7 @@ variable "ipv4_fabric_loopback_pool" {
 variable "ipv4_fabric_address_pool" {
   type    = string
   description = "IPv4 interface address pool for the fabric, i.e. 192.168.100.0/24"
+  default = "192.168.100.0/24"
 
   validation {
     condition      = (length(regexall("^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/[0-9]{1,2}$", var.ipv4_fabric_address_pool))==1)
@@ -69,6 +74,7 @@ variable "ipv4_fabric_address_pool" {
 variable "ipv4_external_address_pool" {
   type    = string
   description = "IPv4 interface addresses pool for the external connections, i.e. 192.168.110.0/24"
+  default = "192.168.110.0/24"
 
   validation {
     condition      = (length(regexall("^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/[0-9]{1,2}$", var.ipv4_external_address_pool))==1)
@@ -79,6 +85,7 @@ variable "ipv4_external_address_pool" {
 variable "ipv4_evpn_loopback_pool" {
   type    = string
   description = "IPv4 EVPN loopback address pool, i.e. 192.168.120.0/24"
+  default = "192.168.120.0/24"
 
   validation {
     condition      = (length(regexall("^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/[0-9]{1,2}$", var.ipv4_evpn_loopback_pool))==1)
@@ -90,6 +97,7 @@ variable "ipv4_evpn_loopback_pool" {
 variable "red_rz_vlan_id" {
   type    = number
   description = "VLAN ID used for RED routing zone"
+  default = 6
 
   validation {
     condition      = var.red_rz_vlan_id < 4001
@@ -100,6 +108,7 @@ variable "red_rz_vlan_id" {
 variable "blue_rz_vlan_id" {
   type    = number
   description = "VLAN ID used for BLUE routing zone"
+  default = 5
 
   validation {
     condition      = var.blue_rz_vlan_id < 4001
@@ -110,20 +119,22 @@ variable "blue_rz_vlan_id" {
 variable "red_rz_vni" {
   type    = number
   description = "VNI used for RED routing zone"
+  default = 6000
 
   validation {
-    condition      = var.red_rz_vni < 16777214
-    error_message = "The VNI must be between 1-16777214"
+    condition      = var.red_rz_vni >= 4096 && var.red_rz_vni <= 16777214
+    error_message = "The VNI must be between 4096-16777214"
   }
 }
 
 variable "blue_rz_vni" {
   type    = number
   description = "VNI used for BLUE routing zone"
+  default = 5000
 
   validation {
-    condition      = var.blue_rz_vni < 16777214
-    error_message = "The VNI must be between 1-16777214"
+    condition      = var.blue_rz_vni >= 4096 && var.blue_rz_vni <= 16777214
+    error_message = "The VNI must be between 4096-16777214"
   }
 }
 
@@ -131,6 +142,7 @@ variable "blue_rz_vni" {
 variable "leaf1_red_to_external_ip" {
   type    = string
   description = "IPv4 address with subnet mask for leaf1 to external gateway in RED VRF, i.e. 10.1.0.0/31"
+  default = "10.1.0.0/31"
 
   validation {
     condition      = (length(regexall("^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/[0-9]{1,2}$", var.leaf1_red_to_external_ip))==1)
@@ -141,6 +153,7 @@ variable "leaf1_red_to_external_ip" {
 variable "external_to_leaf1_red_ip" {
   type    = string
   description = "IPv4 address with subnet mask for external Gateway to leaf1 in RED VRF, i.e. 10.1.0.1/31"
+  default = "10.1.0.1/31"
 
   validation {
     condition      = (length(regexall("^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/[0-9]{1,2}$", var.external_to_leaf1_red_ip))==1)
@@ -152,6 +165,7 @@ variable "external_to_leaf1_red_ip" {
 variable "leaf2_red_to_external_ip" {
   type    = string
   description = "IPv4 address with subnet mask for leaf2 in RED VRF to external gateway, i.e. 10.2.0.0/31"
+  default = "10.2.0.0/31"
 
   validation {
     condition      = (length(regexall("^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/[0-9]{1,2}$", var.leaf2_red_to_external_ip))==1)
@@ -162,6 +176,7 @@ variable "leaf2_red_to_external_ip" {
 variable "external_to_leaf2_red_ip" {
   type    = string
   description = "IPv4 address with subnet mask for external Gateway to leaf2 in RED VRF, i.e. 10.2.0.1/31"
+  default = "10.2.0.1/31"
 
   validation {
     condition      = (length(regexall("^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/[0-9]{1,2}$", var.external_to_leaf2_red_ip))==1)
@@ -173,6 +188,7 @@ variable "external_to_leaf2_red_ip" {
 variable "leaf1_blue_to_external_ip" {
   type    = string
   description = "IPv4 address with subnet mask for leaf1 in BLUE VRF to external gateway, i.e. 10.1.0.0/31"
+  default = "10.1.1.0/31"
 
   validation {
     condition      = (length(regexall("^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/[0-9]{1,2}$", var.leaf1_blue_to_external_ip))==1)
@@ -183,6 +199,7 @@ variable "leaf1_blue_to_external_ip" {
 variable "external_to_leaf1_blue_ip" {
   type    = string
   description = "IPv4 address with subnet mask for external Gateway to leaf1 in BLUE VRF, i.e. 10.1.0.1/31"
+  default = "10.1.1.1/31"
 
   validation {
     condition      = (length(regexall("^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/[0-9]{1,2}$", var.external_to_leaf1_blue_ip))==1)
@@ -194,6 +211,7 @@ variable "external_to_leaf1_blue_ip" {
 variable "leaf2_blue_to_external_ip" {
   type    = string
   description = "IPv4 address with subnet mask for leaf2 in BLUE VRF to external gateway, i.e. 10.2.0.0/31"
+  default = "10.2.1.0/31"
 
   validation {
     condition      = (length(regexall("^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/[0-9]{1,2}$", var.leaf2_blue_to_external_ip))==1)
@@ -204,6 +222,7 @@ variable "leaf2_blue_to_external_ip" {
 variable "external_to_leaf2_blue_ip" {
   type    = string
   description = "IPv4 address with subnet mask for external Gateway to leaf2 in BLUE VRF, i.e. 10.2.0.1/31"
+  default = "10.2.1.1/31"
 
   validation {
     condition      = (length(regexall("^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/[0-9]{1,2}$", var.external_to_leaf2_blue_ip))==1)
